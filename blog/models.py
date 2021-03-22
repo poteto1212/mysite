e8cacb868f101ed2de806eceb5878882c8a1255b
Your Name <you@example.com>
2021-03-22 14:11:02 +0000

    init

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.untils import timezone
 # Create your models here.
+
+class post(models.Model):
+    author=models.ForeignKey(setting.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    
