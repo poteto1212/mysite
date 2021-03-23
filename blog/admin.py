38095b12def944f690aa58efb644d6b9befb28f7
Your Name <you@example.com>
2021-03-23 12:46:29 +0000

    init

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=(
+        'author',
+        'title',
+        'text',
+        'created_date',
+        'published_date',)
+
+
+admin.site.register(Post,PostAdmin)
 
 
 # Register your models here.
